acab533caf2355e592f22413f403c389b77bb639
roman <rubromvit@gmail.com>
2023-06-22 12:30:40 +0300

    init commit

---
+++ src/main/java/ru/rubik/lightdigital/user/entity/User.java
@@ -0,0 +1,14 @@
+package ru.rubik.lightdigital.user.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "password")
+    private String password;
+}
